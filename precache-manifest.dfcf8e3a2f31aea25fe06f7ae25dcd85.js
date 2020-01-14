11caca0c9aec3294c7369d7bdc913e040fbb6637
Khaled Nabil <kn2016a@gmail.com>
2020-01-14 16:16:28 +0200

    Updates

---
+++ precache-manifest.dfcf8e3a2f31aea25fe06f7ae25dcd85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e164a1a79c4d00a1005089e53fe6fa",
+    "url": "/monsters-cards/index.html"
+  },
+  {
+    "revision": "ea5b5413deffa07596b6",
+    "url": "/monsters-cards/static/css/main.6960169f.chunk.css"
+  },
+  {
+    "revision": "662dc7ea351d9823352b",
+    "url": "/monsters-cards/static/js/2.b8eafddd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-cards/static/js/2.b8eafddd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea5b5413deffa07596b6",
+    "url": "/monsters-cards/static/js/main.379b9a26.chunk.js"
+  },
+  {
+    "revision": "48801f4d32644e2d6659",
+    "url": "/monsters-cards/static/js/runtime-main.f04d1b7a.js"
+  }
+]);
